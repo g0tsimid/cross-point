9c3a2039be82be66ff89bbf1b3f49bd5b5e03227
Stephen Tsimidis <stephen.tsimidis@gmail.com>
2017-01-30 16:36:43 -0500

    Update angular version and change style.
    Take up 100% height of the viewport for each column to allow independent scrolling of the different views.

--- src/app/presentation/presentation-editor/code-editor.component.ts
+++ src/app/presentation/presentation-editor/code-editor.component.ts
@@ -3,30 +3,43 @@ import {
   ElementRef,
   EventEmitter,
   Input,
+  OnDestroy,
   OnInit,
   Output
 } from '@angular/core'
 import * as brace from 'brace';
+
 import 'brace/mode/javascript';
 import 'brace/theme/monokai';
 
 /**
- * A code editor that can be made read-only and emits the text in the editor
- * an any selected lines.
+ * A code editor that can be made read-only. It emits changes to the text in 
+ * the editor and to the selected lines.
  */
 @Component({
   selector: "my-code-editor",
   templateUrl: "./code-editor.component.html",
   styleUrls: ["./code-editor.component.css"]
 })
-export class CodeEditorComponent implements OnInit {
+export class CodeEditorComponent implements OnInit, OnDestroy {
+  editor : brace.Editor
+  grabbed : boolean = false
 
   constructor(private elementRef: ElementRef) { }
 
   ngOnInit() {
-    let editor = brace.edit(this.elementRef.nativeElement.querySelector("#editor"));
-    editor.setTheme("ace/theme/monokai");
-    editor.getSession().setMode("ace/mode/javascript");
-    editor.$blockScrolling = Infinity;
+    this.editor = brace.edit(this.elementRef.nativeElement.querySelector(
+      "#editor"));
+    this.editor.setTheme("ace/theme/monokai")
+    this.editor.getSession().setMode("ace/mode/javascript")
+    this.editor.$blockScrolling = Infinity
+  }
+
+  ngOnDestroy() {
+    this.editor.destroy()
+    let grabber : HTMLElement = this.elementRef.nativeElement
+      .querySelector("#vertical-resize-grabber")
+    grabber.removeEventListener("dragstart")
+    grabber.removeEventListener("dragend")
   }
 }
